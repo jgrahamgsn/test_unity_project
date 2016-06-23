86d0fb5c26689f65a60a31b64251a6f96112699c
Jason Graham <jgraham@gsngames.com>
2016-06-23 15:44:10 -0400

    Add preexport and postexport methods. Update .gitignore to ignore sln and csproj files.

---
+++ Unity/Assets/Editor/Test.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Test {
+	public static void PreExport() {
+		Debug.Log (" >>> PreExport Method Invoked");
+	}
+
+	public static void PostExport() {
+		Debug.Log (" >>> PostExport Method Invoked");
+	}
+}
