9d4bfcb3d4a19f43ea364f3f3cd1ffba1714e4e7
Jason Graham <jgraham@gsngames.com>
2016-06-28 14:47:07 -0400

    Add some more callbacks to test

--- Unity/Assets/Editor/Test.cs
+++ Unity/Assets/Editor/Test.cs
@@ -1,12 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public static class Test {
 	public static void PreExport() {
-		Debug.Log (" >>> PreExport Method Invoked");
+		Debug.Log (" >>> PreExport Method Started");
+
+		string currentDirectory = Directory.GetCurrentDirectory ();
+		Debug.Log (" >>> Current Directory: " + currentDirectory);
+
+		Debug.Log (" >>> PreExport Method Ended");
+
 	}
 
 	public static void PostExport() {
-		Debug.Log (" >>> PostExport Method Invoked");
+		Debug.Log (" >>> PostExport Method Started");
+		Debug.Log (" >>> PostExport Method Ended");
+
 	}
 }
---
+++ Unity/Assets/Editor/TestPostProcessor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class TestPostProcessor {
+
+	[PostProcessBuildAttribute]
+	public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject) {
+		Debug.Log (" >>> OnPostProcessBuild started");
+		Debug.Log (" >>> target = " + target.ToString());
+		Debug.Log (" >>> pathtoBuiltProject = " + pathToBuiltProject);
+		Debug.Log (" >>> OnPostProcessBuild ended");
+	}
+
+	[PostProcessSceneAttribute]
+	public static void OnPostprocessScene() { 
+		Debug.Log (" >>> OnPostprocessScene started");
+		Debug.Log (" >>> OnPostprocessScene ended");
+	}
+
+	[DidReloadScripts]
+	public static void OnDidReloadScripts() {
+		Debug.Log (" >>> OnDidReloadScripts started");
+		Debug.Log (" >>> OnDidReloadScripts ended");
+	}
+
+}
