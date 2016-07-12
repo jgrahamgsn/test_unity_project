1119d72547b582ba09dc68683e5599ae316f9744
Jason Graham <jgraham@gsngames.com>
2016-07-12 18:00:31 -0400

    Add some unit tests to figure out how to run and generate some code in the PreExport method to see the result.

--- Unity/Assets/Editor/Test.cs
+++ Unity/Assets/Editor/Test.cs
@@ -1,14 +1,23 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections;
 using System.IO;
 
 public static class Test {
+	[MenuItem("Test/PreExport")]
 	public static void PreExport() {
 		Debug.Log (" >>> PreExport Method Started");
 
 		string currentDirectory = Directory.GetCurrentDirectory ();
 		Debug.Log (" >>> Current Directory: " + currentDirectory);
-
+		string fileClass =
+			"public class Generated { " + "\n" +
+			"    public void Do() {" + "\n" +
+			"        UnityEngine.Debug.Log(\"In Generated.Do!\");" + "\n" +
+			"    }" + "\n" +
+			"}";
+		
+		File.WriteAllText("Assets/Generated.cs", fileClass);
 		Debug.Log (" >>> PreExport Method Ended");
 
 	}
@@ -16,6 +25,5 @@ public static class Test {
 	public static void PostExport() {
 		Debug.Log (" >>> PostExport Method Started");
 		Debug.Log (" >>> PostExport Method Ended");
-
 	}
 }
---
+++ Unity/Assets/Editor/UnitTests.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using NUnit.Framework;
+
+[TestFixture]
+public class UnitTests {
+
+	[Test]
+	public void Test1() {
+		Assert.AreEqual (4, 2 + 2);
+	}
+
+	[Test]
+	public void Test2() {
+		Assert.AreEqual (5, 2 + 2);
+	}
+}
