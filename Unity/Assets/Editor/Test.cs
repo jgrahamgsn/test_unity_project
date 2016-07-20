833f979750de42bed38f2caf8f15788788b26dfd
Jason Graham <jgraham@gsngames.com>
2016-07-20 17:46:56 -0400

    Log out a static value to really prove the methods are run in the same process space.

--- Unity/Assets/Editor/Test.cs
+++ Unity/Assets/Editor/Test.cs
@@ -5,10 +5,16 @@ using System.IO;
 using System.Diagnostics;
 
 public static class Test {
+	public static string TestValue = string.Empty;
+
 	[MenuItem("Test/PreExport")]
 	public static void PreExport() {
 		UnityEngine.Debug.Log (" >>> PreExport Method Started");
 		UnityEngine.Debug.Log (" >>> PreExport Process ID: " + Process.GetCurrentProcess().Id);
+
+		TestValue = "$$$$$$$$$$$$$$$$$";
+		UnityEngine.Debug.Log (" >>> PreExport Static Test Value: " + TestValue);
+
 		string currentDirectory = Directory.GetCurrentDirectory ();
 		UnityEngine.Debug.Log (" >>> Current Directory: " + currentDirectory);
 		string fileClass =
@@ -27,6 +33,7 @@ public static class Test {
 	public static void PostExport() {
 		UnityEngine.Debug.Log (" >>> PostExport Method Started");
 		UnityEngine.Debug.Log (" >>> PostExport Process ID: " + Process.GetCurrentProcess().Id);
+		UnityEngine.Debug.Log (" >>> PostExport Static Test Value: " + TestValue);
 
 		string asset = "Assets/Textures/WikiFun_Police_Smiley.png";
 		Object assetObj = AssetDatabase.LoadMainAssetAtPath (asset);
